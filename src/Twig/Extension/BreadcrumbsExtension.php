66bee427cc17c542a304049e36211a6a567db6dd
Justin Hoffman <justin@ohmedia.ca>
2023-06-19 15:20:39 -0600

    go back to simple breadcrumbs

--- src/Twig/Extension/BreadcrumbsExtension.php
+++ src/Twig/Extension/BreadcrumbsExtension.php
@@ -2,16 +2,25 @@
 
 namespace OHMedia\BootstrapBundle\Twig\Extension;
 
-use OHMedia\BootstrapBundle\Component\Breadcrumbs\BreadcrumbTrail;
+use OHMedia\BootstrapBundle\Component\Breadcrumb;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class BreadcrumbsExtension extends AbstractExtension
 {
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
     public function getFunctions(): array
     {
         return [
+            new TwigFunction('bootstrap_breadcrumb', [$this, 'breadcrumb']),
             new TwigFunction('bootstrap_breadcrumbs', [$this, 'breadcrumbs'], [
                 'is_safe' => ['html'],
                 'needs_environment' => true,
@@ -19,14 +28,17 @@ class BreadcrumbsExtension extends AbstractExtension
         ];
     }
 
-    public function breadcrumbs(Environment $twig, ?BreadcrumbTrail $breadcrumbTrail)
+    public function breadcrumb(string $text, string $route, array $parameters = []): Breadcrumb
     {
-        if (!$breadcrumbTrail) {
-            return '';
-        }
+        $href = $this->urlGenerator->generate($route, $parameters);
 
+        return new Breadcrumb($text, $href);
+    }
+
+    public function breadcrumbs(Environment $twig, Breadcrumb ...$breadcrumbs)
+    {
         return $twig->render('@OHMediaBootstrap/breadcrumbs.html.twig', [
-            'breadcrumb_trail' => $breadcrumbTrail,
+            'breadcrumbs' => $breadcrumbs,
         ]);
     }
 }
