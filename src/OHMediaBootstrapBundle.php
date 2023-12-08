ad647c176e0bf562a821ab38a73c4f80f4d725a4
justin-oh <justin@ohmedia.ca>
2023-12-08 14:34:13 -0600

    simplification

--- src/OHMediaBootstrapBundle.php
+++ src/OHMediaBootstrapBundle.php
@@ -7,8 +7,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OHMediaBootstrapBundle extends Bundle
 {
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-    }
 }
