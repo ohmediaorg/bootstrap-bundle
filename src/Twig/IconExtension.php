971a0c6b5cbdcfa2dcb27f4e5fbf7eacf2ebf277
justin-oh <justin@ohmedia.ca>
2024-03-22 11:39:39 -0600

    Create IconExtension.php

---
+++ src/Twig/IconExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace OHMedia\BootstrapBundle\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class IconExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('bootstrap_icon', [$this, 'icon'], [
+                'is_safe' => ['html'],
+            ]),
+        ];
+    }
+
+    public function icon(string $icon)
+    {
+        return "<i class=\"bi bi-$icon\"></i>";
+    }
+}
