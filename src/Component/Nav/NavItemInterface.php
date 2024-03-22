0b1776d810b186eff517fc9abf46408a2dfeed0c
justin-oh <justin@ohmedia.ca>
2024-03-22 11:52:08 -0600

    Update NavItemInterface.php

--- src/Component/Nav/NavItemInterface.php
+++ src/Component/Nav/NavItemInterface.php
@@ -4,8 +4,6 @@ namespace OHMedia\BootstrapBundle\Component\Nav;
 
 interface NavItemInterface
 {
-    public function getHref(): string;
-
     public function getText(): string;
 
     public function getIcon(): string;
