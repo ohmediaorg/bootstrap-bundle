0b1f429990f7ef7065a49ffa051a207efb1d51a7
justin-oh <justin@ohmedia.ca>
2024-03-28 11:44:48 -0600

    Update Breadcrumb.php

--- src/Component/Breadcrumb.php
+++ src/Component/Breadcrumb.php
@@ -4,22 +4,29 @@ namespace OHMedia\BootstrapBundle\Component;
 
 class Breadcrumb
 {
-    private $href;
     private $text;
+    private $route;
+    private $routeParams;
 
-    public function __construct(string $text, string $href)
+    public function __construct(string $text, string $route = '', array $routeParams = [])
     {
         $this->text = $text;
-        $this->href = $href;
-    }
-
-    public function getHref(): ?string
-    {
-        return $this->href;
+        $this->route = $route;
+        $this->routeParams = $routeParams;
     }
 
     public function getText(): string
     {
         return $this->text;
+    }
+
+    public function getRoute(): string
+    {
+        return $this->route;
+    }
+
+    public function getRouteParams(): array
+    {
+        return $this->routeParams;
     }
 }
