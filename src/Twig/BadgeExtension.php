051eda500e5b68899f556310f8a04edab29a4da2
sean <seanpeterson.997@gmail.com>
2024-04-30 15:17:00 -0600

    Add BadgeExtension Twig Functions

---
+++ src/Twig/BadgeExtension.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace OHMedia\BootstrapBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class BadgeExtension extends AbstractExtension
+{
+    private function badge(string $message, string $themeColor): string
+    {
+        return '<span class="badge text-bg-'.$themeColor.'">'.$message.'</span>';
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('badge_success', [$this, 'badgeSuccess'], [
+                'is_safe' => ['html'],
+            ]),
+            new TwigFunction('badge_info', [$this, 'badgeInfo'], [
+                'is_safe' => ['html'],
+            ]),
+            new TwigFunction('badge_warning', [$this, 'badgeWarning'], [
+                'is_safe' => ['html'],
+            ]),
+        ];
+    }
+
+    public function badgePrimary(string $message): string
+    {
+        return $this->badge($message, 'primary');
+    }
+
+    public function badgeSecondary(string $message): string
+    {
+        return $this->badge($message, 'secondary');
+    }
+
+    public function badgeSuccess(string $message): string
+    {
+        return $this->badge($message, 'success');
+    }
+
+    public function badgeDanger(string $message): string
+    {
+        return $this->badge($message, 'danger');
+    }
+
+    public function badgeWarning(string $message): string
+    {
+        return $this->badge($message, 'warning');
+    }
+
+    public function badgeInfo(string $message): string
+    {
+        return $this->badge($message, 'info');
+    }
+
+    public function badgeLight(string $message): string
+    {
+        return $this->badge($message, 'light');
+    }
+
+    public function badgeDark(string $message): string
+    {
+        return $this->badge($message, 'dark');
+    }
+}
