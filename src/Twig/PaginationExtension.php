359be0739046eb5010419f59c46821b132a96388
Sean Peterson <seanpeterson.997@gmail.com>
2025-11-13 16:11:33 -0600

    Pagination Anchor - Refactor into href
    • Remove JS and instead add to href
    • Rename html_anchor to url_fragment

--- src/Twig/PaginationExtension.php
+++ src/Twig/PaginationExtension.php
@@ -29,7 +29,7 @@ class PaginationExtension extends AbstractExtension
         $options = array_merge([
             'aria_label' => '',
             'size' => 'md',
-            'html_anchor' => null,
+            'url_fragment' => null,
         ], $options);
 
         return $twig->render('@OHMediaBootstrap/pagination.html.twig', [
